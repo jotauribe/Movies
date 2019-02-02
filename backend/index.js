8c697a093c972e68d5174e687462b81215720c50
Jhonatan Uribe <yosoyuribe@gmail.com>
2019-02-02 15:04:51 -0500

    backend - async handler utility

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
-const Express = require('express');
-const app = Express();
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const { asyncHandler } = require('./src/utils');
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello Worl!'));
+app.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const response = await axios.get(
+      `${process.env.TMDB_API_URL}/discover/movie?api_key${
+        process.env.TMDB_API_KEY
+      }`
+    );
+
+    return res.send(response.data);
+  })
+);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
